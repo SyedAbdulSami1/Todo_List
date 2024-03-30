dc21dfe25afc18c5d054ef1d3435ca7d5ca64879
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-30 09:11:44 +0500

    Todo List complete

---
+++ index.ts
@@ -0,0 +1,25 @@
+//#! /usr/bin/env node
+import inquirer from "inquirer";
+let todoList =[];
+let condition = true;
+while (condition) {
+    let input = await inquirer.prompt(
+        [
+            {
+                name:"recElement",
+                message:"What do you want to add in todo List? Pleas Enter: ",
+                type : "string", 
+            },
+            {
+                name:"confirm",
+                message:"Are you want to add? Yes or No",
+                type: "confirm",
+                default:"false",
+            },
+        ]
+    );
+    
+    todoList.push(input.recElement);
+    console.log(todoList);
+    condition = input.confirm;
+}
